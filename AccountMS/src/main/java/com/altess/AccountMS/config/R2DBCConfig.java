ae35caff21bd63ce1b3419bd1c40c13766728925
altmas224 <altmashkhan124@gmail.com>
2023-10-14 15:19:24 +0530

    ALT-3 DB Config for Accont MS

--- AccountMS/src/main/java/com/altess/AccountMS/config/R2DBCConfig.java
+++ AccountMS/src/main/java/com/altess/AccountMS/config/R2DBCConfig.java
@@ -3,12 +3,15 @@ package com.altess.AccountMS.config;
 import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
+import org.springframework.boot.r2dbc.ConnectionFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @Configuration
 @EnableR2dbcAuditing
+@EnableR2dbcRepositories
 public class R2DBCConfig {
 
     @Bean
@@ -20,7 +23,7 @@ public class R2DBCConfig {
                         .option(ConnectionFactoryOptions.PORT, 5432)
                         .option(ConnectionFactoryOptions.USER, "postgres")
                         .option(ConnectionFactoryOptions.PASSWORD, "root")
-                        .option(ConnectionFactoryOptions.DATABASE, "altees")
+                        .option(ConnectionFactoryOptions.DATABASE, "account")
                         .build());
     }
 
